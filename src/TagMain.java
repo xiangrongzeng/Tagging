2ed79bffd4fc8afe410e90e2d6eaca5223cf0a09
sunder <rxiangz@gmail.com>
2015-05-27 09:01:14 +0800

    将数据路径改为了真实的人民日报语料路径

--- src/TagMain.java
+++ src/TagMain.java
@@ -16,13 +16,18 @@ public class TagMain {
 		HashMap<String, Double> confusionMatrix = data.getConfusionMatrix();
 		HashMap<String, HashMap<String, Double>> transformMatrix = data.getTransformMatrix();
 	String[] words = new String[7];
-	words[0] = "建国";
+	words[0] = "123建国";
 	words[1] = "以来";
 	words[2] = "的";
 	words[3] = "第九";
 	words[4] = "任";
 	words[5] = "总统";
 	words[6] = "。";
+	String wordsLine = "";
+	for(String word: words){
+		wordsLine += word;
+	}
+	System.out.println(wordsLine);
 		Viterbi viterbi = new Viterbi(words, confusionMatrix, transformMatrix);
 		viterbi.calc();
 	}
